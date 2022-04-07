f9eea788216f44f612652b8fccfbfa30af802cb8
Mkangor97 <mercykangor@gmail.com>
2022-04-07 12:46:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,35 +1,25 @@
-#include <stdio.h>
+nclude <stdio.h>
+
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
  */
+
 int main(void)
 {
 	char typeChar;
-
-		int typeInt;
-
-			long typeLongInt;
-
-				long long typeLongLongInt;
-
-					float typeFloat;
-
-						/* With sizeof() we get the number of bytes */
-
-						printf("Size of a char: %lu byte(s)\n", sizeof(typeChar));
-
-							printf("Size of an int: %lu byte(s)\n", sizeof(typeInt));
-
-								printf("Size of a long int: %lu byte(s)\n", sizeof(typeLongInt));
-
-									printf("Size of a long long int: %lu byte(s)\n", sizeof(typeLongLongInt));
-
-										printf("Size of a float: %lu byte(s)\n", sizeof(typeFloat));
-
-
-
-											return (0);
-
+	int typeInt;
+	long typeLongInt;
+	long long typeLongLongInt;
+	float typeFloat;
+	/* With sizeof() we get the number of bytes */
+	printf("Size of a char: %lu byte(s)\n", sizeof(typeChar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(typeInt));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(typeLongInt));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(typeLongLongInt));
+	printf("Size of a float: %lu byte(s)\n", sizeof(typeFloat));
+	return (0);
 }
