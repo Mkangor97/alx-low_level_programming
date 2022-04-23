0d3ba08f4a077b8691559c9dd49b479c05969a1f
Mkangor97 <mercykangor@gmail.com>
2022-04-23 06:37:47 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,18 +6,18 @@
  *Return: the encoded string.
  */
 
-char *leet(char *)
+char *leet(char *s)
 {
 	int y = 0, z = 0;
 	char array_leet[] = {'4', '3', '1', '0', '7'};
-	char array_up[] = {'A', 'E', 'O', 'T', 'L'};
-	char array_low[] = {'a', 'e', 'o', 't', 'l'};
+	char array_up[] = {'A', 'E', 'L', 'O', 'T'};
+	char array_low[] = {'a', 'e', 'l', 'o', 't'};
 
 	while (s[y] != '\0')
 	{
 		for (z = 0; z < 5; z++)
 		{
-			if (s[y] == array_low[z] || s[y] == array_up[z])
+			if (s[y] == array_low[z] || s[y] == array_up[j])
 				s[y] = array_leet[z];
 		}
 		y++;
