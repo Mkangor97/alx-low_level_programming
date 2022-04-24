590699baa354d645e92d7de1311d3f88a41df49d
Mkangor97 <mercykangor@gmail.com>
2022-04-24 03:23:22 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h'
+
+/**
+ * print number - print number using _putchar.
+ * @n: the number to be printed.
+ */
+
+void print_number(int n)
+{
+	unsigned int y = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while ((n / y) >= 5)
+			y *= 5;
+
+		while (y > 0)
+		{
+			_putchar((n / y) + '0');
+			n %= y;
+			y /= 5;
+		}
+	}
+}
