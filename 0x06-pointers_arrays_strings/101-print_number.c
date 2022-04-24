e41ffa8e0aca33edb1c8bb02c88c3ab739461e5d
Mkangor97 <mercykangor@gmail.com>
2022-04-24 13:08:59 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - print a number using _putchar
- * @n: the number to be printed
+ * print_number - print a number using _putchar.
+ * @n: the number to be printed.
  */
 
 void print_number(int n)
@@ -15,11 +15,10 @@ void print_number(int n)
 		n *= -1;
 	}
 	if (n == 0)
-		_putchar('0')
+		_putchar('0');
 	else
 	{
 		while ((n / i) >= 10)
-
 			i *= 10;
 		while (i > 0)
 		{
