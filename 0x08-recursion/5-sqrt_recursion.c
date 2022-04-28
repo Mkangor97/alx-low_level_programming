5ce878828eb2ca70fbca9696ea01301ddf457317
Mkangor97 <mercykangor@gmail.com>
2022-04-28 06:04:03 -0700

    function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,13 +8,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int sqrt2(int i, int j)
+int sqrt2(int a, int b)
 {
-	if (j * j == i)
-		return (j);
-	else if (j * j > i)
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
 		return (-1);
-	return (sqrt2(i, j + 1));
+	return (sqrt2(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of n
