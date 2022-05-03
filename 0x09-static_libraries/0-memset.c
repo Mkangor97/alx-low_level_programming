9976a7171b1744e30ece4658147ffdbb4eee1d93
Mkangor97 <mercykangor@gmail.com>
2022-05-03 06:45:47 -0700

    task o static lin

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills string with constant char b upto n bytes
+ * @s: input pointer to string
+ * @b: constant char
+ * @n: number of bytes
+ * Return: pointer to s string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int y = 0;
+
+	while (y < n)
+	{
+		s[y] = b;
+		y++;
+	}
+	return (s);
+}
