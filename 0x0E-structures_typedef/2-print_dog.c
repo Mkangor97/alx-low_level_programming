652886038afd9aa094ca28a09a1c5edbeb9f38ee
Mkangor97 <mercykangor@gmail.com>
2022-05-12 12:20:52 -0700

    unction that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,4 @@
-#innclude "dog.h"
+#include "dog.h"
 #include <stdio.h>
 /**
  * print_dog - prints a struct dog
@@ -11,10 +11,10 @@ void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		if ((*d).name !=0)
+		if ((*d).name != 0)
 			printf("Name: %s\n", (*d).name);
 		else
-		printf("Name: (nil)\n");
+			printf("Name: (nil)\n");
 		printf("Age: %f\n", (*d).age);
 		if ((*d).owner != 0)
 			printf("Owner: %s\n", (*d).owner);
